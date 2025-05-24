b8755dababa158a7a83ff899533211b32b22f809
MinPan9585 <panmin677095@gmail.com>
2025-05-24 12:04:42 +0800

    0524 add player animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,32 +7,64 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     public float dashDistance;
     public float dashSpeed;
-    void Start()
-    {
-
-    }
+    public float dashCD;
+    public Animator anim;
+    bool isDashing = false;
 
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(horizontal, vertical, 0).normalized;
-        transform.Translate(direction * Time.deltaTime * speed);
+        dashCD -=Time.deltaTime;
 
-		if(Input.GetKeyDown("k"))
+        if (!isDashing)
         {
+            Move();
+        }
+		if(Input.GetKeyDown("k") && dashCD<0)
+        {
+
+            anim.SetTrigger("dash");
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+            Vector3 direction = new Vector3(horizontal, vertical, 0).normalized;
             Vector3 dashTarget = transform.position + direction * dashDistance;
             StartCoroutine(Dash(dashTarget));
+            dashCD = 2f;
         }
-		
     }
     
+    void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        if (horizontal > 0)
+        {
+            transform.GetChild(0).localScale = Vector3.one * 0.4f;
+        }
+        else if (horizontal < 0)
+        {
+            transform.GetChild(0).localScale = new Vector3(-1, 1, 1) * 0.4f;
+        }
+        if (horizontal == 0 && vertical == 0)
+        {
+            anim.SetBool("isMoving", false);
+        }
+        if (horizontal != 0 || vertical != 0)
+        {
+            anim.SetBool("isMoving", true);
+        }
+        Vector3 direction = new Vector3(horizontal, vertical, 0).normalized;
+        transform.Translate(direction * Time.deltaTime * speed);
+    }
+
     IEnumerator Dash(Vector3 dashTarget)
     {
         while (Vector3.Distance(transform.position, dashTarget) > 0.1f)
         {
-            transform.position = Vector3.Lerp(transform.position, dashTarget, dashSpeed);
+            isDashing = true;
+            //transform.position = Vector3.Lerp(transform.position, dashTarget, dashSpeed);
+            transform.position = Vector3.MoveTowards(transform.position, dashTarget, dashSpeed);
             yield return null;
         }
+        isDashing = false;
     }
 }
